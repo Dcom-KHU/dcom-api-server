af909f02347e4f7e8d0b5d2a1d0c41ac5dde0026
justkode <sobu0715@gmail.com>
2022-03-04 20:10:24 +0900

    fix: User Profile Dto method

--- src/main/java/dcom/homepage/api/domain/users/dto/UserProfileDto.java
+++ src/main/java/dcom/homepage/api/domain/users/dto/UserProfileDto.java
@@ -1,9 +1,13 @@
 package dcom.homepage.api.domain.users.dto;
 
+import dcom.homepage.api.domain.users.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter @Setter
@@ -15,4 +19,21 @@ public class UserProfileDto {
     private final String realName;
     private final String github;
     private final String homepage;
+
+    public static UserProfileDto of(User user) {
+        return UserProfileDto.builder()
+                .id(user.getId())
+                .userId(user.getUserId())
+                .admissionYear(user.getAdmissionYear())
+                .realName(user.getRealName())
+                .github(user.getGithub())
+                .homepage(user.getHomepage())
+                .build();
+    }
+
+    public static List<UserProfileDto> of(List<User> userProfileDtoList) {
+        return userProfileDtoList.stream()
+                .map(UserProfileDto::of)
+                .collect(Collectors.toList());
+    }
 }
