869cf273dc3961c4cab76ad085ce0a500ff51ca1
justkode <sobu0715@gmail.com>
2022-03-30 15:26:51 +0900

    feat: added Abstract Class StudyCommit

---
+++ src/main/java/dcom/homepage/api/domain/items/StudyCommit.java
@@ -0,0 +1,33 @@
+package dcom.homepage.api.domain.items;
+
+import dcom.homepage.api.domain.study.Study;
+import dcom.homepage.api.domain.users.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class StudyCommit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date summitDate;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Study study;
+}
