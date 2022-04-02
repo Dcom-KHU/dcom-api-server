10a771bc987fe6a89fe317a30a3735d2c17f8c3b
justkode <sobu0715@gmail.com>
2022-04-03 01:24:32 +0900

    feat: findAll method in StudyCommitRepository

--- src/main/java/dcom/homepage/api/domain/studycommits/repository/StudyCommitRepository.java
+++ src/main/java/dcom/homepage/api/domain/studycommits/repository/StudyCommitRepository.java
@@ -1,7 +1,17 @@
 package dcom.homepage.api.domain.studycommits.repository;
 
+import dcom.homepage.api.domain.study.Study;
 import dcom.homepage.api.domain.studycommits.StudyCommit;
+import dcom.homepage.api.domain.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 public interface StudyCommitRepository<T extends StudyCommit> extends JpaRepository<T, Long> {
+    List<T> findAllByUser(User user);
+    List<T> findAllByStudy(Study study);
+    List<T> findAllByUserAndStudy(User user, Study study);
+    List<T> findAllByUserAndSummitDate(User user, Date summitDate);
 }
